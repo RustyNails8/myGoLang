3e25932b95c0c322c22f2a0a7e4cec813587b2e1
Sumit Das <sumit8das@gmail.com>
2020-05-22 04:57:41 +0530

    Hello

---
+++ cmd/hello/go.mod
@@ -0,0 +1,5 @@
+module example.com/in10c2/hello
+
+go 1.14
+
+require github.com/google/go-cmp v0.4.1
---
+++ cmd/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/in10c2/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
