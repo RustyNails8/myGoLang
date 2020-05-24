01bd22d3bbc1f442d5ee1f4105c9d48d3c227e04
Sumit Das <sumit8das@gmail.com>
2020-05-25 03:41:06 +0530

    Improved UserInput for err printing

--- cmd/userinput/userinput.go
+++ cmd/userinput/userinput.go
@@ -3,13 +3,34 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Print("Enter the server name:   ")
+	fmt.Print("Enter the score:   ")
 	// reader := bufio.NewReader(os.Stdin)
 	// LPAR, err := reader.ReadString('\n')
-	LPAR, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Print(LPAR)
+	score, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	score = strings.TrimSpace(score)
+
+	grade, err := strconv.ParseFloat(score, 64)
+	if err != nil {
+		fmt.Println("\n\nWould you please enter a number ?")
+		fmt.Println("Please rerun the program !\n\n I got this error : ")
+		log.Fatal(err)
+	}
+
+	if grade == 100 {
+		fmt.Println(" Perfect Score !")
+	} else if grade >= 40 {
+		fmt.Println(" Passed !")
+	} else if grade < 40 {
+		fmt.Println(" Failed !")
+	}
 }
