b95675d3559a0771a5acb1944a169e4da8ff7c67
Sumit Das <sumit8das@gmail.com>
2020-05-25 02:44:42 +0530

    Now I know how to handle err in userinput

--- cmd/userinput/userinput.go
+++ cmd/userinput/userinput.go
@@ -10,6 +10,6 @@ func main() {
 	fmt.Print("Enter the server name:   ")
 	// reader := bufio.NewReader(os.Stdin)
 	// LPAR, err := reader.ReadString('\n')
-	LPAR, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Print(LPAR, "RC:", err)
+	LPAR, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Print(LPAR)
 }
